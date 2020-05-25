f4972c1e7adbc38dfee3d94bd978ad347b10d6ef
saraclaeys <sara.claeys01@gmail.com>
2020-05-25 12:56:24 +0200

    story

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,10 +6,10 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            Console.WriteLine("There once was a man named John");
+            Console.WriteLine("He was 35 years old");
+            Console.WriteLine("He really liked the name John");
+            Console.WriteLine("But didn't like being 35");
 
             Console.ReadLine();
         }

21e91a2ea3d45073f8ee5ce1663741587f7cb143
saraclaeys <sara.claeys01@gmail.com>
2020-05-25 16:12:21 +0200

    arrays

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,11 +6,14 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " you are " + age);
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42}; 
+            string[] friends = new string[5];
+            friends [0] = "Jim";
+            friends [1] = "Kelly";
+
+            luckyNumber[1] = 900;
+
+            Console.WriteLine ( luckyNumbers[1] );
 
             Console.ReadLine();
         }

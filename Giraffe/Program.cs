b849c11b143487d3dceaa7654ea43340c2746a63
saraclaeys <sara.claeys01@gmail.com>
2020-05-25 13:30:53 +0200

    datatypes

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,16 +6,9 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string characterName = "Tom";
-            int characterAge;
-            characterAge = 25;
-
-            Console.WriteLine("There once was a man named " + characterName);
-            Console.WriteLine("He was " + characterAge +" years old");
-
-            characterName = "Mike";
-            Console.WriteLine("He really liked the name " + characterName);
-            Console.WriteLine("But didn't like being " + characterAge);
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
 
             Console.ReadLine();
         }

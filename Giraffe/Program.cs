4339a0ca34b63aaa04dccdeca7a3c6c52ed1ea13
saraclaeys <sara.claeys01@gmail.com>
2020-05-26 12:05:24 +0200

    GetMax method

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,27 +6,27 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            bool isMale = false;
-            bool isTall = false;
+            Console.WriteLine(GetMax(20, 10, 40));
+     
+            Console.ReadLine();
+        }
 
-            if (isMale && isTall)
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+
+            if (num1 >= num2 && num1 >= num3)
             {
-                Console.WriteLine("You are a tall male");
-
-            } else if (isMale && !isTall)
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3)
             {
-                Console.WriteLine("You are a short male");
-
-            } else if (!isMale && isTall)
-            {
-                Console.WriteLine("You are not a male but you are tall");
-
+                result = num2;
             } else
             {
-                Console.WriteLine("You are not male and not tall");
+                result = num3;
             }
 
-            Console.ReadLine();
+            return result;
         }
 
    

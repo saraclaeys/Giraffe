5990d92de9d99d6c90e5afcf55aee8b3eacacc4f
saraclaeys <sara.claeys01@gmail.com>
2020-05-26 10:27:59 +0200

    objects: say hi

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,16 +6,15 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42}; 
-            string[] friends = new string[5];
-            friends [0] = "Jim";
-            friends [1] = "Kelly";
-
-            luckyNumber[1] = 900;
-
-            Console.WriteLine ( luckyNumbers[1] );
+            SayHi("Sara");
 
             Console.ReadLine();
+        }
+
+        static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+
         }
     }
 }

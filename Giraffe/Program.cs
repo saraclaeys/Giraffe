774aac055be0408714465a1775bb06e29e061115
saraclaeys <sara.claeys01@gmail.com>
2020-06-01 17:43:26 +0200

    loop through array

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,16 +6,11 @@ namespace Giraffe
     {
         private static void Main(string[] args)
         {
-            int i = 1;
-            while (i <= 5)
-            {
-                Console.WriteLine(i);
-                i++;
-            }
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
 
-            for (int i = 1; i <= 5; i++)
+            for (int i = 0; i <= luckyNumbers.Length; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(luckyNumbers[i]);
             }
 
             Console.ReadLine();

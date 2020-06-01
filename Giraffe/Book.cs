e582670cb2f29a940bd9c856259c344d40ddff57
saraclaeys <sara.claeys01@gmail.com>
2020-06-01 18:55:08 +0200

    new class Book

---
+++ Giraffe/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giraffe
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}

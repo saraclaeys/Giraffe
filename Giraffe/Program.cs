1c7b8f02f3101a5da50d6d6ac91c9351cc7db5b2
saraclaeys <sara.claeys01@gmail.com>
2020-06-03 15:11:56 +0200

    class Book and new objects

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,7 +6,17 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
+            Book book1 = new Book(); // new object
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
 
+            Book book2 = new Book(); // new object
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book2.author);
 
             Console.ReadLine();
         }
